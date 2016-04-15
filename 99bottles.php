3c117c9b213f6b36cf7dff7b6a8252aa2129ad92
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-15 15:36:49 -0500

    made seperate file for 99bottles finished challenge

---
+++ 99bottles.php
@@ -0,0 +1,13 @@
+<?php
+start:
+$bottles = 99;
+while ($bottles > 0) {
+    sleep(1);
+    echo "{$bottles} ".$amount." of beer on the wall\n";
+    $amount = ($bottles == 2 || $bottles == 0) ? "bottle" : "bottles";
+    $bottles--;
+    echo "take one down pass it around {$bottles} " .$amount." of beer on the wall\n";
+
+}
+echo "No more bottles of beer on the wall time to go to the\n";
+goto start;
--- while.php
+++ while.php
@@ -1,15 +1,7 @@
 <?php
 
-// $test = 5;
-// while($test <= 15) {
-//     echo "{$test}\n";
-//     $test++;
-// }
-
-$bottles = 99;
-while ($bottles > 0) {
-    echo "{$bottles} " . $amount . " of beer on the wall\n";
-    $amount = ($bottles == 2 || $bottles == 0) ? "bottle" : "bottles";
-    $bottles--;
-    echo "take one down pass it around {$bottles} " . $amount . " of beer on the wall\n";   
+$test = 5;
+while($test <= 15) {
+    echo "{$test}\n";
+    $test++;
 }
