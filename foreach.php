7a5e5b0b7345504e5eb95ceb2b2ace5cc3f96153
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-18 15:23:12 -0500

    final foreach check nested array

--- foreach.php
+++ foreach.php
@@ -2,7 +2,11 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $value) {
-    if (is_scalar($value)) {
-        echo "{$value} is scalar\n";
+    if (is_array($value)) {
+        foreach ($value as $data) {
+            echo $data.PHP_EOL;
+        }
+    } else {
+        echo $value.PHP_EOL;
     }
 }
