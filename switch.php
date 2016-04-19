420149e6bb3a584aa0643402fb071eaa077aeaaf
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-19 11:45:34 -0500

    working

--- switch.php
+++ switch.php
@@ -1,32 +1,31 @@
  <?php
 
- // Set the default timezone
- date_default_timezone_set('America/Chicago');
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
 
- // Get Day of Week as number
- // 1 (for Monday) through 7 (for Sunday)
- $dayOfWeek = date('N');
-
- switch($dayOfWeek) {
-     case 1:
-         echo "Monday\n";
-         break;
-     case 2:
-         echo "Tuesday\n";
-         break;
-     case 3:
-         echo "Tuesday\n";
-         break;
-     case 4:
-         echo "Tuesday\n";
-         break;
-     case 5:
-         echo "Tuesday\n";
-         break;
-     case 6: 
-         echo "Tuesday\n";
-         break;
-     case 7:
-         echo "Tuesday\n";
-         break;
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$dayOfWeek = date('N');
+switch($dayOfWeek) {
+    case 1:
+        echo "Monday\n";
+        break;
+    case 2:
+        echo "Tuesday\n";
+        break;
+    case 3:
+        echo "Tuesday\n";
+        break;
+    case 4:
+        echo "Tuesday\n";
+        break;
+    case 5:
+        echo "Tuesday\n";
+        break;
+    case 6: 
+        echo "Tuesday\n";
+        break;
+    case 7:
+        echo "Tuesday\n";
+        break;
 }
