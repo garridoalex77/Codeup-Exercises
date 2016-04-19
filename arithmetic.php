d11dcd4a1834f47637a8acdb77b3e25fe0cad493
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-19 15:39:49 -0500

    added numeric check and error message

--- arithmetic.php
+++ arithmetic.php
@@ -2,27 +2,49 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a + $b;
+    } else {
+        return "Error both arguments must be numbers, value \$a : {$a}, \$b : {$b}";
+    }
 }
 function subtract($a, $b)
 {
-    return $a - $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a - $b;
+    } else {
+        return "Error both arguments must be numbers, value \$a : {$a}, \$b : {$b}";
+    }
 }
 function multiply($a, $b)
 {
-   return $a * $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a * $b;
+    } else {
+        return "Error both arguments must be numbers, value \$a : {$a}, \$b : {$b}";
+    }
 }
 function divide($a, $b)
 {
-    return $a / $b;
+    if ($b == 0) {
+        return "don't divide by 0, value \$a : {$a}, \$b : {$b}";
+    } elseif (is_numeric($a) && is_numeric($b)) {
+        return $a / $b;
+    } else {
+        return "Error both arguments must be numbers, value \$a : {$a}, \$b : {$b}";
+    }
 }
 function modulus($a, $b)
 {
-    return $a % $b;
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a % $b;
+    } else {
+        return "Error both arguments must be numbers, value \$a : {$a}, \$b : {$b}";
+    }
 }
 // Add code to test your functions here
-echo add(42, 21).PHP_EOL;
+echo add('dog', 21).PHP_EOL;
 echo subtract(65, 33).PHP_EOL;
-echo divide(99, 33).PHP_EOL;
-echo multiply(55, 5).PHP_EOL;
+echo multiply(3, 3).PHP_EOL;
+echo divide(99, 0).PHP_EOL;
 echo modulus(55, 7).PHP_EOL;
