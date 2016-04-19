2722f452ba8cc2cb8765ad136f4cdf435fc88abd
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-19 13:42:06 -0500

    added code and called each function

--- arithmetic.php
+++ arithmetic.php
@@ -4,20 +4,20 @@ function add($a, $b)
 {
     return $a + $b;
 }
-
+echo add(42, 21).PHP_EOL;
 function subtract($a, $b)
 {
     return $a - $b;
 }
-
+echo subtract(65, 33).PHP_EOL;
 function multiply($a, $b)
 {
    return $a * $b;
 }
-
+echo multiply(55, 5).PHP_EOL;
 function divide($a, $b)
 {
     return $a / $b;
 }
-
+echo divide(99, 33).PHP_EOL;
 // Add code to test your functions here
