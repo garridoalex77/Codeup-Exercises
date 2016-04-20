61d0fd144464e556e889ef01bc0334fdba71741e
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-20 14:12:48 -0500

    compare arrays and count comparisons

--- search-arrays.php
+++ search-arrays.php
@@ -15,3 +15,12 @@ if (array_search('Tina', $names) !== false) {
 } else {
     echo "False\n";
 }
+
+$i = 0;
+foreach ($names as $index => $name) {
+    if ($name == $compare[$index]) {
+        $i++;
+        echo "{$name} is in both arrays\n";
+    }
+}
+        echo "{$i} matches between arrays\n";
