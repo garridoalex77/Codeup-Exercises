8a844c815bec260a70dc1ff56617e88958d960ac
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-21 21:18:35 -0500

    added dashes to numbers

--- contact-parser.php
+++ contact-parser.php
@@ -1,7 +1,6 @@
 <?php
 
-function parseContacts($filename)
-{
+function parseContacts($filename) {
     $contactList = [];
     $contacts = [];
     $handle = fopen($filename, 'r');
@@ -10,19 +9,17 @@ function parseContacts($filename)
     $contacts = explode("\n", $contents);
     array_pop($contacts);
     foreach ($contacts as $data) {
-
         if (!is_numeric($data)) {
             $personalInfo = array();
             $personalInfo["name"] = $data;
         } else {
+            var_dump($data);
+            $data = substr($data, 0, 3) . "-" . substr($data, 3, 3) . "-" . substr($data, 6, 4);
             $personalInfo["number"] = $data;
             array_push($contactList, $personalInfo);
         }
-
     }
-
     fclose($handle);
     return $contactList;
 }
-
 var_dump(parseContacts('contacts.txt'));
