7453daf04193138bccb772813ebf989d11049ac5
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-22 14:28:16 -0500

    writes to new file

--- logger.php
+++ logger.php
@@ -1,9 +1,19 @@
 <?php
 
 function logMessage($logLevel, $message) {
+    $date = date("Y-m-d");
+    $time = date('h:i:s');
+    $fileName = "log-{$date}.log";
     
-
+    $handle = fopen($fileName, "a");
+    fwrite($handle, "{$date} {$time} {$logLevel} {$message}\n");
 }
+logInfo("This is an info message.");
+logError("This is an info message.");
 
-logMessage("INFO", "This is an info message.");
-logMessage("ERROR", "This is an info message.");
+function logInfo($message){
+    logMessage("INFO", $message);
+}
+function logError($message){
+    logMessage("ERROR", $message);
+}
