f398800bb07fd2e82cd363d51368771ecb4bb0c8
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-02 13:48:18 -0500

    constructor

--- log.php
+++ log.php
@@ -1,18 +1,37 @@
 <?php 
-class Log {
-    public function logMessage($logLevel, $message) {
+class Log 
+{
+    public $fileName;
+    public $handle;
+    public $prefix;
+    public function __construct($prefix = "log") 
+    {
+       $this->prefix = $prefix;
+    }
+
+    public function logMessage($logLevel, $message) 
+    {
         date_default_timezone_set("UTC");
         $date = date('Y-m-d');
         $time = date('h:i:s');
-        $fileName = "log-{$date}.log";
-        $handle = fopen($fileName, "a");
+        $this->fileName = $this->prefix."-{$date}.log";
+        $handle = fopen($this->fileName, "a");
         fwrite($handle, "{$date} {$time} {$logLevel} {$message}\n");
         fclose($handle);
     }
-    public function logInfo($message){
+
+    public function logInfo($message) 
+    {
         $this->logMessage("INFO", $message);
     }
-    public function logError($message){
+
+    public function logError($message) 
+    {
         $this->logMessage("ERROR", $message);
+    }
+
+    public function __destruct() 
+    {
+
     }
 }
--- log_test.php
+++ log_test.php
@@ -1,8 +1,7 @@
 <?php
 require_once "Log.php";
 
-
-$test = new Log();
+$test = new Log('cli');
 $test->logInfo("This is an info message.");
 $test->logError("This is an info message.");
-
+// var_dump($test);
