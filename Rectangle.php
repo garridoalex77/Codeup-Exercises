af3fac7cb1ce5c161fa0c71e90d9763b9179fcbd
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-17 14:14:56 -0500

    using setters and getters

--- Rectangle.php
+++ Rectangle.php
@@ -4,21 +4,33 @@ class Rectangle {
     private $height;
     private $width;
 
-    public function getHeight() 
-    {
-        return $this->height;
-    }
-    public function getWidth() 
-    {
-        return $this->width;
-    }
-
     public function __construct($height, $width) 
     {
         $this->height = $height;
         $this->width = $width;
 
     }
+
+    private function setHeight($height) 
+    {
+        $this->height = trim($height);
+    }
+    
+    private function setWidth($width) 
+    {
+        $this->width = trim($width);
+    }
+    
+    public function getHeight() 
+    {
+        return $this->height;
+    }
+    
+    public function getWidth() 
+    {
+        return $this->width;
+    }
+
 
     public function perimeter() 
     {
